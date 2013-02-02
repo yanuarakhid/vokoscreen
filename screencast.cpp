231fad6fe31ea12896e15caf640cf3463da89338
Volker Kohaupt <vk@cvolker.fritz.box>
2013-02-02 18:35:15 +0100

    test 2

--- screencast.cpp
+++ screencast.cpp
@@ -80,7 +80,7 @@ screencast::screencast()
       Beta = "";
 
     ProgName = "vokoscreen";
-    Version = "1.4.7";
+    Version = "1.4.8";
     Version = Version + " " + Beta;
     email = "<a href ='mailto:tux@vodafone.de?subject=vokoscreen ";
     email = email.append( Version ).append( "'" ).append( ">tux@vodafone.de</a>" );
