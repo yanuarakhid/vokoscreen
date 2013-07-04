b4addaf9f0bab45c89275dc41c3ee31274dd28dd
Mariusz Fik <fisiu@opensuse.org>
2013-07-04 23:17:04 +0200

    Update for all translations - done with lupdate. Polish translation completed.
    Signed-off-by: Mariusz Fik <fisiu@opensuse.org>

--- language/vokoscreen_de_DE.ts
+++ language/vokoscreen_de_DE.ts
@@ -2,14 +2,63 @@
 <!DOCTYPE TS>
 <TS version="2.0" language="de_DE">
 <context>
+    <name>QvkMail</name>
+    <message>
+        <source>Mail last Video</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>You find the Video as attachment</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Select one or more files</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Mail one or more selected Video</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Send</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QvkWebcam</name>
+    <message>
+        <source>Noborder</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Border</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Close</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QvkWebcamController</name>
+    <message>
+        <source>Webcam</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Device is busy</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
     <name>screencast</name>
     <message>
         <source>Compress:</source>
-        <translation>Komprimieren:</translation>
+        <translation type="obsolete">Komprimieren:</translation>
     </message>
     <message>
         <source>Mouse cursor not recording</source>
-        <translation>Mauszeiger nicht aufnehmen</translation>
+        <translation type="obsolete">Mauszeiger nicht aufnehmen</translation>
     </message>
     <message>
         <source>Videopath</source>
@@ -37,7 +86,7 @@
     </message>
     <message>
         <source>Play</source>
-        <translation>Play</translation>
+        <translation type="unfinished">Play</translation>
     </message>
     <message>
         <source>Open Directory</source>
@@ -53,7 +102,83 @@
     </message>
     <message>
         <source>webcam settings</source>
-        <translation>webcam Einstellungen</translation>
+        <translation type="obsolete">webcam Einstellungen</translation>
+    </message>
+    <message>
+        <source>Audio</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Alsa</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Pulse</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Frames:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Player</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Recording time</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Aktual frames per second</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Size in KB</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Codec</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Format</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Device </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source> is busy</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>%1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Default</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Do not record mouse cursor</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Pause</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Send</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Recording screensize</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Settings fps</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
