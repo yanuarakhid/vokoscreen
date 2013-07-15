728ff3c1760f76e86ae04fecc04ac3b69a42120d
Volker Kohaupt <vk@cvolker.fritz.box>
2013-07-16 00:02:47 +0200

    added language it_CH

---
+++ language/vokoscreen_it_CH.ts
@@ -0,0 +1,75 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="it_CH">
+<context>
+    <name>screencast</name>
+    <message>
+        <source>Compress:</source>
+        <translation>Comprimere:</translation>
+    </message>
+    <message>
+        <source>Mouse cursor not recording</source>
+        <translation>Non registrare il cursore del mouse </translation>
+    </message>
+    <message>
+        <source>Videopath</source>
+        <translation>Percorso Video</translation>
+    </message>
+    <message>
+        <source>Fullscreen</source>
+        <translation>Schermo intero</translation>
+    </message>
+    <message>
+        <source>Window</source>
+        <translation>Finestra</translation>
+    </message>
+    <message>
+        <source>Area</source>
+        <translation>Area</translation>
+    </message>
+    <message>
+        <source>Start</source>
+        <translation>Avvia</translation>
+    </message>
+    <message>
+        <source>Stop</source>
+        <translation>Ferma</translation>
+    </message>
+    <message>
+        <source>Play</source>
+        <translation>Riproduci</translation>
+    </message>
+    <message>
+        <source>Open Directory</source>
+        <translation>Apri cartella</translation>
+    </message>
+    <message>
+        <source>Magnification</source>
+        <translation>Lente d&apos;ingrandimento</translation>
+    </message>
+    <message>
+        <source>Vokoscreen minimized when recording starts</source>
+        <translation>Minimizza Vokoscreen quando inizia la registrazione</translation>
+    </message>
+    <message>
+        <source>player</source>
+        <translation>Riproduttore</translation>
+    </message>
+    <message>
+        <source>frames</source>
+        <translation>fotogrammi</translation>
+    </message>
+    <message>
+        <source>codec</source>
+        <translation>codec</translation>
+    </message>
+    <message>
+        <source>standard</source>
+        <translation>standard</translation>
+    </message>
+    <message>
+        <source>webcam settings</source>
+        <translation>Impostazioni webcam</translation>
+    </message>
+</context>
+</TS>
