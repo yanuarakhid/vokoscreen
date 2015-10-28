09ee38d1903f94a145e880508f071d96ff8c2e30
Volker Kohaupt <vk@cvolker.fritz.box>
2015-10-28 17:30:39 +0100

    add Portuguese translation

---
+++ language/vokoscreen_pt.ts
@@ -0,0 +1,314 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="pt" version="2.0">
+<context>
+    <name>AlsaBusyDialog</name>
+    <message>
+        <source>Device is busy</source>
+        <translation>Dispositivo ocupado</translation>
+    </message>
+    <message>
+        <source>Name</source>
+        <translation>Nome</translation>
+    </message>
+    <message>
+        <source>Unknown</source>
+        <translation>Desconhecido</translation>
+    </message>
+    <message>
+        <source>Device</source>
+        <translation>Dispositivo</translation>
+    </message>
+    <message>
+        <source>Used by</source>
+        <translation>Usado por</translation>
+    </message>
+</context>
+<context>
+    <name>MailDialog</name>
+    <message>
+        <source>Mail last Video</source>
+        <translation>Enviar o último Video</translation>
+    </message>
+    <message>
+        <source>Mail one or more selected Video</source>
+        <translation>Enviar um ou mais vídeos selecionados</translation>
+    </message>
+    <message>
+        <source>Send</source>
+        <translation>Enviar</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Cancelar</translation>
+    </message>
+</context>
+<context>
+    <name>NoPlayerDialog</name>
+    <message>
+        <source>Please install a videoplayer</source>
+        <translation>Por favor instale um videoplayer</translation>
+    </message>
+</context>
+<context>
+    <name>QvkMail</name>
+    <message>
+        <source>You find the Video as attachment</source>
+        <translation>Você encontra o vídeo como anexo</translation>
+    </message>
+    <message>
+        <source>Select one or more files</source>
+        <translation>Selecione um ou mais ficheiros</translation>
+    </message>
+</context>
+<context>
+    <name>QvkWebcamWindow</name>
+    <message>
+        <source>Border</source>
+        <translation>Margem</translation>
+    </message>
+    <message>
+        <source>Close</source>
+        <translation>Fechar</translation>
+    </message>
+    <message>
+        <source>Show over fullscreen</source>
+        <translation>Mostrar sobre o ecrã inteiro</translation>
+    </message>
+    <message>
+        <source>Right click for menu</source>
+        <translation>Clique direito para o menu</translation>
+    </message>
+    <message>
+        <source>User-defined</source>
+        <translation>Definido pelo utilizador</translation>
+    </message>
+</context>
+<context>
+    <name>WebcamBusyDialog</name>
+    <message>
+        <source>Device is busy</source>
+        <translation>Dispositivo ocupado</translation>
+    </message>
+    <message>
+        <source>Name</source>
+        <translation>Nome</translation>
+    </message>
+    <message>
+        <source>Device</source>
+        <translation>Dispositivo</translation>
+    </message>
+    <message>
+        <source>Used by</source>
+        <translation>Usado por</translation>
+    </message>
+    <message>
+        <source>Unknown</source>
+        <translation>Desconhecido</translation>
+    </message>
+</context>
+<context>
+    <name>regionselection</name>
+    <message>
+        <source>doubleclick extended to the edge</source>
+        <translation>duplo clique estendido ao bordo</translation>
+    </message>
+</context>
+<context>
+    <name>screencast</name>
+    <message>
+        <source>Fullscreen</source>
+        <translation>Ecrã inteiro</translation>
+    </message>
+    <message>
+        <source>Window</source>
+        <translation>Janela</translation>
+    </message>
+    <message>
+        <source>Area</source>
+        <translation>Área</translation>
+    </message>
+    <message>
+        <source>Magnification</source>
+        <translation>Ampliação</translation>
+    </message>
+    <message>
+        <source>Audio</source>
+        <translation>Áudio</translation>
+    </message>
+    <message>
+        <source>Alsa</source>
+        <translation>Alsa</translation>
+    </message>
+    <message>
+        <source>Pulse</source>
+        <translation>Pulse</translation>
+    </message>
+    <message>
+        <source>Default</source>
+        <translation>Defeito</translation>
+    </message>
+    <message>
+        <source>Codec</source>
+        <translation>Codec</translation>
+    </message>
+    <message>
+        <source>Do not record mouse cursor</source>
+        <translation>Não gravar o cursor do rato</translation>
+    </message>
+    <message>
+        <source>Videopath</source>
+        <translation>Caminho do Video </translation>
+    </message>
+    <message>
+        <source>Player</source>
+        <translation>Leitor</translation>
+    </message>
+    <message>
+        <source>Vokoscreen minimized when recording starts</source>
+        <translation>Vokoscreen minimiza quando a gravação começar</translation>
+    </message>
+    <message>
+        <source>Start</source>
+        <translation>Começar</translation>
+    </message>
+    <message>
+        <source>Stop</source>
+        <translation>Parar</translation>
+    </message>
+    <message>
+        <source>Pause</source>
+        <translation>Pausa</translation>
+    </message>
+    <message>
+        <source>Play</source>
+        <translation>Tocar</translation>
+    </message>
+    <message>
+        <source>Send</source>
+        <translation>Enviar</translation>
+    </message>
+    <message>
+        <source>Recording time</source>
+        <translation>Tempo de gravação</translation>
+    </message>
+    <message>
+        <source>Size in KB</source>
+        <translation>Tamanho em KB</translation>
+    </message>
+    <message>
+        <source>Recording screensize</source>
+        <translation>Gravar com tamanho de ecrã</translation>
+    </message>
+    <message>
+        <source>Format</source>
+        <translation>Formato</translation>
+    </message>
+    <message>
+        <source>Settings fps</source>
+        <translation>Definições de fps</translation>
+    </message>
+    <message>
+        <source>Open Directory</source>
+        <translation>Abrir Directorio</translation>
+    </message>
+    <message>
+        <source>Countdown</source>
+        <translation>Contagem</translation>
+    </message>
+    <message>
+        <source>Play last Video</source>
+        <translation>Tocar o último Video</translation>
+    </message>
+    <message>
+        <source>Send Video</source>
+        <translation>Enviar Video</translation>
+    </message>
+    <message>
+        <source>Select one or more devices</source>
+        <translation>Selecione um ou mais dispositivos</translation>
+    </message>
+    <message>
+        <source>Actual frames per second</source>
+        <translation>Frames reais por segundo</translation>
+    </message>
+    <message>
+        <source>Go</source>
+        <translation>Avançar</translation>
+    </message>
+    <message>
+        <source>Homepage</source>
+        <translation>Página Inicial</translation>
+    </message>
+    <message>
+        <source>Support</source>
+        <translation>Suporte</translation>
+    </message>
+    <message>
+        <source>Developer</source>
+        <translation>Desenvolvedor</translation>
+    </message>
+    <message>
+        <source>Frames</source>
+        <translation>Frames</translation>
+    </message>
+    <message>
+        <source>Donate</source>
+        <translation>Doar</translation>
+    </message>
+    <message>
+        <source>Translations</source>
+        <translation>Traduções</translation>
+    </message>
+    <message>
+        <source>Sourcecode</source>
+        <translation>Código fonte</translation>
+    </message>
+    <message>
+        <source>Show in systray</source>
+        <translation>Mostrar no systray</translation>
+    </message>
+    <message>
+        <source>New version available</source>
+        <translation>Nova versão disponível</translation>
+    </message>
+    <message>
+        <source>Videocodec</source>
+        <translation>Codec de Video</translation>
+    </message>
+    <message>
+        <source>Audiocodec</source>
+        <translation>Codec de Som</translation>
+    </message>
+    <message>
+        <source>Webcam</source>
+        <translation>Camera Web</translation>
+    </message>
+    <message>
+        <source>Select webcam</source>
+        <translation>Selecionar a camara web</translation>
+    </message>
+    <message>
+        <source>Mirrored</source>
+        <translation>Espelhado</translation>
+    </message>
+    <message>
+        <source>Display</source>
+        <translation>Exibir</translation>
+    </message>
+    <message>
+        <source>All Displays</source>
+        <translation>Todas as exibições</translation>
+    </message>
+    <message>
+        <source>Developer Homepage</source>
+        <translation>Página Pessoal do Desenvolvedor</translation>
+    </message>
+    <message>
+        <source>Recorder</source>
+        <translation>Gravador</translation>
+    </message>
+    <message>
+        <source>Select recorder</source>
+        <translation>Selecionar o gravador</translation>
+    </message>
+</context>
+</TS>
