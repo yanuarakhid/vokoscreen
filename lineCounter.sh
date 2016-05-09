da7e7fe39934d6f4b792990390583b88b4fbefa5
Volker Kohaupt <vk@cvolker.fritz.box>
2016-05-09 22:19:58 +0200

    linecounter

--- lineCounter.sh
+++ lineCounter.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
- 
-find . -name '*.cpp' -type f -print0 | xargs -0 cat | wc -l
-find . -name '*.h' -type f -print0 | xargs -0 cat | wc -l
-find . -name '*.ts' -type f -print0 | xargs -0 cat | wc -l
+
+cpp=$( find . -name '*.cpp' -type f -print0 | xargs -0 cat | wc -l )
+echo "cpp:" $cpp
+
+h=$( find . -name '*.h' -type f -print0 | xargs -0 cat | wc -l )
+echo "h  :" $h
+
+ts=$( find . -name '*.ts' -type f -print0 | xargs -0 cat | wc -l )
+echo "ts :" $ts
+
