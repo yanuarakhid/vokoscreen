58c9a8101365b6d29444b3d7735f1d0a78c1ae6f
Volker Kohaupt <vkohaupt@freenet.de>
2018-03-31 20:16:22 +0200

    example-dbus

--- example-dbus.sh
+++ example-dbus.sh
@@ -1,4 +1,17 @@
 #!/bin/bash 
 
+./vokoscreen&
+sleep 10
+
+
+#exsample with qdbus
 qdbus org.vokoscreen.screencast /record org.vokoscreen.screencast.vokoscreenInterface.setWebcamOn
 
+sleep 10
+
+#example with dbus-send
+dbus-send --session --type=method_call --dest=org.vokoscreen.screencast /record org.vokoscreen.screencast.vokoscreenInterface.setWebcamOff
+
+
+#show all method
+qdbus org.vokoscreen.screencast /record | grep vokoscreen
