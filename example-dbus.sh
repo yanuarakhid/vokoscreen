992ebe382a4a572a87a4a445fd5aa4164082c5ce
Volker Kohaupt <vkohaupt@freenet.de>
2018-04-03 14:53:29 +0200

    edit exsample-dbus.sh

--- example-dbus.sh
+++ example-dbus.sh
@@ -1,10 +1,18 @@
 #!/bin/bash 
 
+# exsample qdbus
+# qdbus org.vokoscreen.screencast /gui org.vokoscreen.gui.setWebcamOn
+
+# example with dbus-send, it is the same as qdbus but a other command
+# dbus-send --session --type=method_call --dest=org.vokoscreen.screencast /gui org.vokoscreen.gui.setWebcamOn
+
+
 # Please adjust path to vokoscreen
 # and start in background
 ./vokoscreen&
 
 
+# Now we have to wait until vokoscreen is fully loaded
 value=""
 while [ "$value" = "" ]
 do
@@ -14,18 +22,16 @@ do
 done
 
 
-# exsample with qdbus
-qdbus org.vokoscreen.screencast /gui org.vokoscreen.gui.setAudioOn
+# Start webcam
+qdbus org.vokoscreen.screencast /gui org.vokoscreen.gui.setWebcamOn
 
 
 # Show webcam 5 second
-#sleep 5
+sleep 5
 
 
 # Now we close the webcam
-# example with dbus-send, it is the same as qdbus but an other command
-#dbus-send --session --type=method_call --dest=org.vokoscreen.screencast /gui org.vokoscreen.gui.setAudiOff
-qdbus org.vokoscreen.screencast /gui org.vokoscreen.gui.setAudioOff
+qdbus org.vokoscreen.screencast /gui org.vokoscreen.gui.setWebcamOff
 
 
 # show all method
